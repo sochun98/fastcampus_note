63dcf8ea6b46a9df4a67fd291a9b7408c10d5508
sochun98 <sochun98@gmail.com>
2024-07-10 13:30:46 +0900

    [feat] sochun98 20240710

--- app/apis/users_api.py
+++ app/apis/users_api.py
@@ -2,4 +2,4 @@ from django.http import JsonResponse
 
 
 def test_api(reqeust):
-    return JsonResponse(status=200, data=dict(result="ok"))
+    return JsonResponse(status=200, data=dict(result="패스트캠퍼스"), json_dumps_params={"ensure_ascii": False})
